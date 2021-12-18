b6baf3f27194ab254c65d4e0c455b1e410cd293f
Jonathan Day <jonday@jonday.dev>
2021-12-18 13:36:53 -0600

    add donation form from square

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -1,21 +1,10 @@
-import { useRef, useState, useEffect } from "react";
+import { useEffect } from "react";
 
 import styles from "./Donate.module.css";
 
 import Card from "../UI/Card";
 
 const Donate = (props) => {
-  const amountInput = useRef();
-  const [amtToDonate, setAmtToDonate] = useState(0);
-
-  const amountHandler = (e) => {
-    setAmtToDonate(e.target.value);
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-  };
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -29,28 +18,7 @@ const Donate = (props) => {
 
   return (
     <Card className={styles.donate}>
-      <p>
-        The Brownwood Art Association welcomes any and all donations to help
-        with operational costs, current and upcoming projects. Donations are tax
-        deductible and can be made online.
-      </p>
-      <h2>Make A Donation to the Brownwood Art Association</h2>
-      <form className={styles.form} onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="amount">Donation Amount</label>
-          <span className={styles.dollar}>$</span>
-          <input
-            type="number"
-            min="1"
-            step="1"
-            ref={amountInput}
-            value={amtToDonate}
-            onChange={amountHandler}
-            className={styles.input}
-          />
-        </div>
-        <button className={styles["donate-btn"]}>Donate {amtToDonate > 0 && '$'}{amtToDonate}</button>
-      </form>
+      <iframe src="https://square.link/u/nBLV2h9F" title="order-form" className={styles.iform}/>
     </Card>
   );
 };
