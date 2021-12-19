63a06bf2e3b6668422b214bbaafbbf646c16af9a
Jonathan Day <jonday@jonday.dev>
2021-12-19 14:02:54 -0600

    create classes page

--- src/components/OnlineClasses/Classes.js
+++ src/components/OnlineClasses/Classes.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 import styles from "./Classes.module.css";
 
@@ -18,7 +18,39 @@ const Classes = (props) => {
 
   return (
     <Card className={styles.card}>
-      <h1>Classes page coming soon</h1>
+      <h2>Welcome to your next inspiration!</h2>
+      <h3>
+        Our new video series will feature a well-curated selection of artists
+        and mediums. ​Highlighting a lifetime’s worth of skill in a short,
+        enjoyable format. We will be adding new content often with some of the
+        demos becoming in-person workshops and classes!
+      </h3>
+      <h2>Come learn with us!</h2>
+      <div className={styles["block-one"]}>
+        <h4 className={styles["block-title"]}>
+          Palette Knife Demo 1 & 2 - Christine Brisley
+        </h4>
+        <div className={styles.vids}>
+          <iframe
+            width="200"
+            height="180"
+            src="https://www.youtube.com/embed/Wte3rdje2q0"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="200"
+            height="180"
+            src="https://www.youtube.com/embed/lKkZkKW5Irg"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
     </Card>
   );
 };
