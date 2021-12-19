5237bc4fae0a18337e0b53ec344cd03e890a8377
Jonathan Day <jonday@jonday.dev>
2021-12-19 09:40:33 -0600

    change some images

--- src/components/Layout/Features.js
+++ src/components/Layout/Features.js
@@ -4,10 +4,10 @@ import styles from "./Features.module.css";
 
 import vid from "../../assets/vid.png";
 import demos from "../../assets/demos.png";
-import hasBegun from "../../assets/hasBegun.png";
-import merlot from "../../assets/merlot.png";
-import anniversary from "../../assets/anniversary.jpg";
-import galleries from "../../assets/galleries.jpg";
+// import hasBegun from "../../assets/hasBegun.png";
+// import merlot from "../../assets/merlot.png";
+// import anniversary from "../../assets/anniversary.jpg";
+// import galleries from "../../assets/galleries.jpg";
 
 import Card from "../UI/Card";
 
@@ -26,12 +26,14 @@ const Features = (props) => {
 
   return (
     <Card className={styles.features}>
-      <img src={vid} alt="videos" />
+      <button><img src={vid} alt="videos" /></button>
+      <button><img src={demos} alt="videos" /></button>
+      {/* <img src={vid} alt="videos" />
       <img src={demos} alt="videos" />
       <img src={hasBegun} alt="videos" />
       <img src={merlot} alt="videos" />
       <img src={anniversary} alt="videos" />
-      <img src={galleries} alt="videos" />
+      <img src={galleries} alt="videos" /> */}
     </Card>
   );
 };
--- src/components/Layout/WhatsNew.js
+++ src/components/Layout/WhatsNew.js
@@ -1,12 +1,13 @@
 import styles from './WhatsNew.module.css';
-import bannerLogo from '../../assets/logo_banner.jpg';
+// import bannerLogo from '../../assets/logo_banner.jpg';
+import hasBegun from '../../assets/hasBegun.png';
 import backdrop from '../../assets/backdrop.jpg';
 
 import Card from '../UI/Card';
 
 const WhatsNew = props => {
     return <Card className={styles.main}>
-        <img src={bannerLogo} alt="logo" className={styles['logo-img']}/>
+        <img src={hasBegun} alt="logo" className={styles['logo-img']}/>
         <h2>WHAT'S NEW AT THE BROWNWOOD ART ASSOCIATION</h2>
         <img src={backdrop} alt="art center" className={styles['art-center-img']} />
     </Card>
