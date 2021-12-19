9ca4442397b62102be4bd7dfdad7b16bca024b8a
Jonathan Day <jonday@jonday.dev>
2021-12-19 09:15:39 -0600

    change image on WhatsNew component

--- src/components/Layout/WhatsNew.js
+++ src/components/Layout/WhatsNew.js
@@ -1,6 +1,6 @@
 import styles from './WhatsNew.module.css';
 import bannerLogo from '../../assets/logo_banner.jpg';
-import artcenter from '../../assets/artcenter.jpg';
+import backdrop from '../../assets/backdrop.jpg';
 
 import Card from '../UI/Card';
 
@@ -8,7 +8,7 @@ const WhatsNew = props => {
     return <Card className={styles.main}>
         <img src={bannerLogo} alt="logo" className={styles['logo-img']}/>
         <h2>WHAT'S NEW AT THE BROWNWOOD ART ASSOCIATION</h2>
-        <img src={artcenter} alt="art center" className={styles['art-center-img']} />
+        <img src={backdrop} alt="art center" className={styles['art-center-img']} />
     </Card>
 }
 
