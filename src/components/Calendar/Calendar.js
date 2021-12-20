c4434bc3bceb2232c23761ab08b907586b61a8b7
Jonathan Day <jonday@jonday.dev>
2021-12-20 09:03:47 -0600

    fix calendar to show agenda instead of month

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -4,7 +4,7 @@ import Card from "../UI/Card";
 
 const Calendar = props => {
     return <Card className={styles.calendar}>
-        <iframe className={styles.iframe} title="calendar" src="https://calendar.google.com/calendar/embed?src=gsoihrrad14tjsaoio05glnag0%40group.calendar.google.com&ctz=America%2FChicago" style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+        <iframe className={styles.iframe} title="calendar" src="https://calendar.google.com/calendar/embed?src=gsoihrrad14tjsaoio05glnag0%40group.calendar.google.com&ctz=America%2FChicago&mode=AGENDA" style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
     </Card>
 }
 
