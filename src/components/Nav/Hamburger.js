6a2ae6a84ca4348045b17955a33909ebbf30e384
Jonathan Day <jonday@jonday.dev>
2021-12-20 06:18:40 -0600

    seperate hamburger menu from navbar component

---
+++ src/components/Nav/Hamburger.js
@@ -0,0 +1,104 @@
+import styles from './Hamburger.module.css';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { pageActions } from '../../store/index';
+
+const Hamburger = (props) => {
+  const dispatch = useDispatch();
+  const showMenu = useSelector((state) => state.page.showMenu);
+
+  const homeHandler = () => {
+    dispatch(pageActions.home());
+    props.onClick();
+  };
+
+  const aboutHandler = () => {
+    dispatch(pageActions.about());
+    props.onClick();
+  };
+
+  const donateHandler = () => {
+    dispatch(pageActions.donate());
+    props.onClick();
+  };
+
+  const contactHandler = () => {
+    dispatch(pageActions.contact());
+    props.onClick();
+  };
+
+  const calendarHandler = () => {
+    dispatch(pageActions.calendar());
+    props.onClick();
+  };
+
+  const membershipHandler = () => {
+    dispatch(pageActions.membership());
+    props.onClick();
+  };
+
+  const contestHandler = () => {
+    dispatch(pageActions.contest());
+    props.onClick();
+  };
+
+  const videoHandler = () => {
+    dispatch(pageActions.video());
+    props.onClick();
+  };
+
+  return (
+    <ul
+      className={`${styles["small-menu"]} ${
+        showMenu ? styles.fit : styles.hide
+      }`}
+    >
+      <li>
+        <button onClick={homeHandler} className={styles["small-menu__btn"]}>
+          Home
+        </button>
+      </li>
+      <li>
+        <button onClick={aboutHandler} className={styles["small-menu__btn"]}>
+          About Us
+        </button>
+      </li>
+      <li>
+        <button onClick={contactHandler} className={styles["small-menu__btn"]}>
+          Contact
+        </button>
+      </li>
+      <li>
+        <button onClick={calendarHandler} className={styles["small-menu__btn"]}>
+          Calendar
+        </button>
+      </li>
+      <li>
+        <button onClick={donateHandler} className={styles["small-menu__btn"]}>
+          Donate
+        </button>
+      </li>
+      <li>
+        <button
+          onClick={membershipHandler}
+          className={styles["small-menu__btn"]}
+        >
+          Membership
+        </button>
+      </li>
+      <li>
+        <button onClick={contestHandler} className={styles["small-menu__btn"]}>
+          2022 Contest
+        </button>
+      </li>
+      <li>
+        <button onClick={videoHandler} className={styles["small-menu__btn"]}>
+          Videos
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default Hamburger;
--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -3,125 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import styles from "./Navbar.module.css";
 import logo from "../../assets/logo.png";
 
+import Hamburger from './Hamburger';
+
 import { MdClose } from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
 
 import { pageActions } from "../../store/index";
-
-const Hamburger = (props) => {
-  const dispatch = useDispatch();
-  const showMenu = useSelector((state) => state.page.showMenu);
-
-  const homeHandler = () => {
-    dispatch(pageActions.home());
-    props.onClick();
-  };
-
-  const aboutHandler = () => {
-    dispatch(pageActions.about());
-    props.onClick();
-  };
-
-  const donateHandler = () => {
-    dispatch(pageActions.donate());
-    props.onClick();
-  };
-
-  const contactHandler = () => {
-    dispatch(pageActions.contact());
-    props.onClick();
-  };
-
-  const calendarHandler = () => {
-    dispatch(pageActions.calendar());
-    props.onClick();
-  };
-
-  const membershipHandler = () => {
-    dispatch(pageActions.membership());
-    props.onClick();
-  };
-
-  const contestHandler = () => {
-    dispatch(pageActions.contest());
-    props.onClick();
-  };
-
-  const videoHandler = () => {
-    dispatch(pageActions.video());
-    props.onClick();
-  };
-
-  return (
-    
-        <ul
-          className={`${styles["small-menu"]} ${showMenu ? styles.fit : styles.hidden}`}
-        >
-          <li>
-            <button onClick={homeHandler} className={styles["small-menu__btn"]}>
-              Home
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={aboutHandler}
-              className={styles["small-menu__btn"]}
-            >
-              About Us
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={contactHandler}
-              className={styles["small-menu__btn"]}
-            >
-              Contact
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={calendarHandler}
-              className={styles["small-menu__btn"]}
-            >
-              Calendar
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={donateHandler}
-              className={styles["small-menu__btn"]}
-            >
-              Donate
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={membershipHandler}
-              className={styles["small-menu__btn"]}
-            >
-              Membership
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={contestHandler}
-              className={styles["small-menu__btn"]}
-            >
-              2022 Contest
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={videoHandler}
-              className={styles["small-menu__btn"]}
-            >
-              Videos
-            </button>
-          </li>
-        </ul>
-  )}
-    
-  
 
 const Navbar = (props) => {
   const dispatch = useDispatch();
