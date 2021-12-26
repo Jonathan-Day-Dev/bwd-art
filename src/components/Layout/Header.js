5c077c5cc950db00ae9e4e306b8473d2c4219f5c
Jonathan Day <jonday@jonday.dev>
2021-12-26 13:24:04 -0600

    add image to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,21 @@
-import styles from './Header.module.css';
-import logo from '../../assets/logo.png'
+import styles from "./Header.module.css";
+import logo from "../../assets/logo.png";
 
-const Header = props => {
-    return <header className={styles.header}>
-        <div className={styles.top}>
-            <img src={logo} alt="logo" className={styles.logo} />
-            <h1>Brownwood Art Association</h1>
-        </div>
+const Header = (props) => {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="logo" className={styles.logo} />
+      <div className={styles.top}>
+        <h1>Brownwood Art Association</h1>
         <div className={styles.info}>
-            <p>P.O. Box 782</p>
-            <p>Brownwood, Texas 76804</p>
-            <p>(325) 641-2916</p>
+          <p>P.O. Box 782</p>
+          <p>215 Fisk Ave</p>
+          <p>Brownwood, Texas 76804</p>
+          <p>(325) 641-2916</p>
         </div>
+      </div>
     </header>
-}
+  );
+};
 
 export default Header;
