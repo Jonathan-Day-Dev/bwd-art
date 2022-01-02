60d4c51c4c7f9d9e6a774302d0e70cc1309b7f9f
Jonathan Day <jonday@jonday.dev>
2022-01-02 16:45:38 -0600

    notice that I did something with Nav but cant remember what it is...

--- src/components/Nav/Hamburger.js
+++ src/components/Nav/Hamburger.js
@@ -1,58 +1,61 @@
 import styles from "./Hamburger.module.css";
 
 import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 
 const Hamburger = (props) => {
   return (
-    <ul
-      className={`${styles["small-menu"]} ${
-        props.isOpen ? styles.fit : styles.hide
-      }`}
-    >
-      <li>
-        <Link
-          to="/home"
-          onClick={props.onToggle}
-          className={styles["small-menu__btn"]}
-        >
-          Home
-        </Link>
-      </li>
-      <a
-        href="https://brownwood-art.square.site/"
-        onClick={props.onToggle}
-        className={styles["small-menu__btn"]}
+    <CSSTransition>
+      <ul
+        className={`${styles["small-menu"]} ${
+          props.isOpen ? styles.fit : styles.hide
+        }`}
       >
-        Events
-      </a>
-      <li>
-        <Link
-          to="/calendar"
-          onClick={props.onToggle}
-          className={styles["small-menu__btn"]}
-        >
-          Calendar
-        </Link>
-      </li>
-      <li>
+        <li>
+          <Link
+            to="/home"
+            onClick={props.onToggle}
+            className={styles["small-menu__btn"]}
+          >
+            Home
+          </Link>
+        </li>
         <a
-          href="https://square.link/u/nBLV2h9F"
+          href="https://brownwood-art.square.site/"
           onClick={props.onToggle}
           className={styles["small-menu__btn"]}
         >
-          Donate
+          Events
         </a>
-      </li>
-      <li>
-        <a
-          href="https://square.link/u/ytQ3nnnH"
-          className={styles["small-menu__btn"]}
-          onClick={props.onToggle}
-        >
-          Membership
-        </a>
-      </li>
-    </ul>
+        <li>
+          <Link
+            to="/calendar"
+            onClick={props.onToggle}
+            className={styles["small-menu__btn"]}
+          >
+            Calendar
+          </Link>
+        </li>
+        <li>
+          <a
+            href="https://square.link/u/nBLV2h9F"
+            onClick={props.onToggle}
+            className={styles["small-menu__btn"]}
+          >
+            Donate
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://square.link/u/ytQ3nnnH"
+            className={styles["small-menu__btn"]}
+            onClick={props.onToggle}
+          >
+            Membership
+          </a>
+        </li>
+      </ul>
+    </CSSTransition>
   );
 };
 
