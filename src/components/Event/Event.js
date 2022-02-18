fe82db4aa522e3ee51c649b45557252be84e9b9c
Jonathan Day <jonday@jonday.dev>
2022-02-18 16:54:34 -0600

    made event page dynamic

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,68 +1,94 @@
 import styles from "./Event.module.css";
 import portrait from "../../assets/portrait.jpg";
+import marchExhibit from "../../assets/march_image.jpg";
 
-import { useEffect } from 'react';
+import { useEffect, useState } from "react";
 
 import Card from "../UI/Card";
+import Info from "./components/Info";
+
+const events = [
+  {
+    title: "Portrait Workshop",
+    date: "February 21-23, 2022",
+    time: "9:30 am to 3:00 pm",
+    img: portrait,
+    price: 95,
+    instructor: {
+      name: "Brenda Shaw",
+      phone: "(325) 642-0417",
+      email: "regency1298@gmail.com",
+    },
+    description:
+      "The workshop will be an introduction to painting the portrait in oil paint. The oil portrait training will focus on painting the face in one session using the Zorn palette in a traditional manner.",
+    supplies: [
+      "Zorn limited palette for oil",
+      "Black, White, Yellow Ochre, Cadmium Red Light",
+      "12\"x12\" the Fine Touch canvas, avaliable at Hobby Lobby",
+      "Master's Touch brush for Oil & Acrylic",
+      "#10 -filber #8 flat #4 flat"
+    ]
+  },
+  {
+    title: "March Water Color Exhibit",
+    date: "February 28, 2022",
+    time: "2 pm to 6 pm",
+    img: marchExhibit,
+    description:
+      "The Brownwood Art Association's March exhibit will feature works of water media done by Association members. Water media include watercolors, gouache, and acrylics on paper. Works submitted must be framed and wired to hang. There is no charge to submit your work into the show. Art may be offered for sale. ",
+  },
+];
 
 const Event = (props) => {
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 450,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   };
 
   useEffect(() => {
-      scrollToTop();
-  }, [])
+    scrollToTop();
+  }, []);
+
+  const eventSelect = (index) => {
+    setSelectedEvent(events[index]);
+  };
+
+  const clearEvents = () => {
+    setSelectedEvent(null);
+  }
+
   return (
     <Card className={styles.card}>
-      <img src={portrait} alt={portrait} className={styles.image} />
-      <h2>Portrait Workshop</h2>
-      <div className={styles.info}>
-        <div>
-          <p>
-            <em>benefiting the Brownwood Art Association</em>
-          </p>
-          <p>The Art Center, 215 Fisk Avenue</p>
-          <p>Brownwood, TX</p>
-          <p>February 21-23, 2022</p>
-          <p>9:30 am to 3:00 pm</p>
-          <p>$95 signup</p>
-        </div>
-        <div>
-          <p>Instructor - Brenda Shaw</p>
-          <p>(325) 642-0417</p>
-          <a href="mailto:regency1298@gmail.com" className={styles.email}>
-            Email
-          </a>
-        </div>
-        <p className={styles.desc}>
-          The workshop will be an introduction to painting the portrait in oil
-          paint. The oil portrait training will focus on painting the face in
-          one session using the Zorn palette in a traditional manner.
-        </p>
-        <div>
-          <h4>Supplies</h4>
-          <ul>
-            <li>
-              <p>Zorn limited palette for oil</p>
-              <p>Black, White, Yellow Ochre, Cadmium Red Light</p>
+      {selectedEvent && (
+        <Info
+          title={selectedEvent.title}
+          date={selectedEvent.date}
+          time={selectedEvent.time}
+          img={selectedEvent.img}
+          price={selectedEvent.price}
+          instructor={selectedEvent.instructor}
+          desc={selectedEvent.description}
+          supplies={selectedEvent.supplies}
+          onClear={clearEvents}
+        />
+      )}
+      {!selectedEvent && (
+        <ul>
+          {events.map((event, i) => (
+            <li key={event.title}>
+              <button className={styles.btn} onClick={() => eventSelect(i)}>
+                <h2>{event.title}</h2>
+                <span>{event.date}</span>
+                <span>{event.time}</span>
+              </button>
             </li>
-            <li>
-              <p>12"x12" the Fine Touch canvas, available at Hobby Lobby</p>
-            </li>
-            <li>
-              <p>Master's Touch brush for Oil & Acrylic</p>
-              <p>#10 -filbert #8 flat #4 flat</p>
-              <p>
-                <em>If you have these items, bring your own</em>
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
+          ))}
+        </ul>
+      )}
     </Card>
   );
 };
---
+++ src/components/Event/components/Info.js
@@ -0,0 +1,53 @@
+import styles from "./Info.module.css";
+import portrait from "../../../assets/portrait.jpg";
+
+import { Fragment } from "react";
+
+const Info = (props) => {
+  return (
+    <Fragment>
+      <div className="relative w-full">
+        <button className={styles["back-btn"]} onClick={props.onClear}>X</button>
+        <h2 className="text-center">{props.title}</h2>
+      </div>
+      <img src={props.img} alt={portrait} className={styles.image} />
+      <div className={styles.info}>
+        <div>
+          <p>
+            <em>benefiting the Brownwood Art Association</em>
+          </p>
+          <p>The Art Center, 215 Fisk Avenue</p>
+          <p>Brownwood, TX</p>
+          <p>{props.date}</p>
+          <p>{props.time}</p>
+          {props.price && <p>$95 signup</p>}
+        </div>
+        {props.instructor && (
+          <div>
+            <p>Instructor - {props.instructor.name}</p>
+            <p>{props.instructor.phone}</p>
+            <a
+              href={`mailto:${props.instructor.email}`}
+              className={styles.email}
+            >
+              Email
+            </a>
+          </div>
+        )}
+      </div>
+      <p className={styles.desc}>{props.desc}</p>
+      {props.supplies && (
+        <div>
+          <h4 className="mt-4 text-xl font-semibold">Supplies</h4>
+          <ul>
+            {props.supplies.map((item) => (
+              <li key={item} className={styles["list-item"]}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default Info;
