3566e3d3d75be64adb4d7894c5405dc1c7aab4cc
Jonathan Day <jonday@jonday.dev>
2022-03-01 10:44:43 -0600

    change images

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,8 +1,8 @@
 import styles from "./Event.module.css";
-import marchExhibit from "../../assets/march_image.jpg";
+import ducks01 from "../../assets/ducks01.jpg";
+import ducks02 from "../../assets/ducks02.jpg";
 import marchExhibit2 from "../../assets/march_image02.jpg";
 import textile from "../../assets/textile_img.png";
-import sailboat from "../../assets/sailboat.jpg";
 
 import { useState, useEffect, useRef } from "react";
 import Box from "@mui/material/Box";
@@ -17,22 +17,6 @@ import CustomCard from "../UI/CustomCard";
 
 const events = [
   {
-    title: "Water Media Exhibit - Intake",
-    date: "February 28, 2022",
-    time: "2 pm to 6 pm",
-    img: marchExhibit,
-    description:
-      "The Brownwood Art Association's March exhibit will feature works of water media done by Association members. Water media include watercolors, gouache, and acrylics on paper. Works submitted must be framed and wired to hang. There is no charge to submit your work into the show. Art may be offered for sale. The exhibit will remain open throughout March.",
-    benefit: false,
-    instructor: {
-      name: "Brenda Shaw",
-      phone: "(325) 642-0417",
-      email: "regency1298@gmail.com",
-      isInstructor: false,
-    },
-    price: 200
-  },
-  {
     title: "Water Media Exhibit - Reception",
     date: "March 3, 2022",
     time: "5 pm to 8 pm",
@@ -45,7 +29,7 @@ const events = [
     title: "En Plein Air Brownwood Competition - Entry",
     date: "April 2, 2022",
     time: "7:30 am to 9 am",
-    img: sailboat,
+    img: ducks01,
     description:
       'The event is open to all artists, age 18 and older. Registration will be at the Art Center. Blank canvases will be stamped at this time. Ending time and return to the Art Center will be at 4 pm the same day. One or two plein art artworks created during the event and one studio artwork created at any time of any subject except nudes may be entered in the competition by each artist for a total of 3 works. All works accepted must remain at The Art Center for exhibit and sale through April 23, 2022. Length and width of any frame may not exceed 26". All work must be sturdily framed or gallery wrapped and ready to hang. All work must be for sale. Artists will receive 70% of the sale price of the work, with BAA receieving 30%.',
     benefit: false,
@@ -55,7 +39,7 @@ const events = [
     title: "En Plein Air Brownwood Competition - Exhibit",
     date: "April 7, 2022",
     time: "5 pm to 8 pm",
-    img: sailboat,
+    img: ducks02,
     description:
       "The award ceremony will begin at 6 pm and the exhibit will remain open from April 7 to April 22",
     benefit: false,
