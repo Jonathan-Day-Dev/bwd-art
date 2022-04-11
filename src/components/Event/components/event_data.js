efd6650831cc7eaa78790718702a651bfb153f24
Jonathan Day <jonday@jonday.dev>
2022-04-11 17:20:00 -0500

    remove en plein air stuff

--- src/components/Event/components/event_data.js
+++ src/components/Event/components/event_data.js
@@ -1,26 +1,26 @@
-import ducks01 from "../../../assets/ducks01.jpg";
-import ducks02 from "../../../assets/ducks02.jpg";
+// import ducks01 from "../../../assets/ducks01.jpg";
+// import ducks02 from "../../../assets/ducks02.jpg";
 
 const events = [
-    {
-      title: "En Plein Air Brownwood Competition - Entry",
-      date: "April 2, 2022",
-      time: "7:30 am to 9 am",
-      img: ducks01,
-      description:
-        'The event is open to all artists, age 18 and older. Registration will be at the Art Center. Blank canvases will be stamped at this time. Ending time and return to the Art Center will be at 4 pm the same day. One or two plein art artworks created during the event and one studio artwork created at any time of any subject except nudes may be entered in the competition by each artist for a total of 3 works. All works accepted must remain at The Art Center for exhibit and sale through April 23, 2022. Length and width of any frame may not exceed 26". All work must be sturdily framed or gallery wrapped and ready to hang. All work must be for sale. Artists will receive 70% of the sale price of the work, with BAA receieving 30%.',
-      benefit: false,
-      price: "$20/entry",
-    },
-    {
-      title: "En Plein Air Brownwood Competition - Exhibit",
-      date: "April 7, 2022",
-      time: "5 pm to 8 pm",
-      img: ducks02,
-      description:
-        "The award ceremony will begin at 6 pm and the exhibit will remain open from April 7 to April 22",
-      benefit: false,
-    },
+    // {
+    //   title: "En Plein Air Brownwood Competition - Entry",
+    //   date: "April 2, 2022",
+    //   time: "7:30 am to 9 am",
+    //   img: ducks01,
+    //   description:
+    //     'The event is open to all artists, age 18 and older. Registration will be at the Art Center. Blank canvases will be stamped at this time. Ending time and return to the Art Center will be at 4 pm the same day. One or two plein art artworks created during the event and one studio artwork created at any time of any subject except nudes may be entered in the competition by each artist for a total of 3 works. All works accepted must remain at The Art Center for exhibit and sale through April 23, 2022. Length and width of any frame may not exceed 26". All work must be sturdily framed or gallery wrapped and ready to hang. All work must be for sale. Artists will receive 70% of the sale price of the work, with BAA receieving 30%.',
+    //   benefit: false,
+    //   price: "$20/entry",
+    // },
+    // {
+    //   title: "En Plein Air Brownwood Competition - Exhibit",
+    //   date: "April 7, 2022",
+    //   time: "5 pm to 8 pm",
+    //   img: ducks02,
+    //   description:
+    //     "The award ceremony will begin at 6 pm and the exhibit will remain open from April 7 to April 22",
+    //   benefit: false,
+    // },
   ];
 
   export default events;
