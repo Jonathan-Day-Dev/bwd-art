393fe197f234c4f6525371b5dc6086e50c85180f
Jonathan Day <jonday@jonday.dev>
2022-05-18 10:03:46 -0500

    make size changes to Program components

--- src/components/Event/Programs.js
+++ src/components/Event/Programs.js
@@ -32,7 +32,7 @@ const Program = (props) => {
     >
       <CustomCard
         className={styles["textile-card"]}
-        maxWth={{ xs: "80%", md: "30%" }}
+        maxWth={{ xs: "80%", md: "80%" }}
         title="The Art of Fiber"
         body={
           <div>
@@ -50,7 +50,7 @@ const Program = (props) => {
               traditional arts into a contemporary environment where they can
               inspire and grow in relation to our modern contemporary world.
             </p>
-            <img src={bowl} alt="textile artists" className="mb-4 w-full" />
+            <img src={bowl} alt="textile artists" className="mb-4 w-[50%]" />
             <p className="leading-tight">
               There are many textile artists in our community, and our goal is
               to bring them together and provide a forum that inspires and
--- src/components/Event/components/event_data.js
+++ src/components/Event/components/event_data.js
@@ -1,41 +1,72 @@
-// import ducks01 from "../../../assets/ducks01.jpg";
-// import ducks02 from "../../../assets/ducks02.jpg";
-// import clay from "../../../assets/clay.jpg";
-
 const events = [
   // {
-  //   title: "Self-Drying Clay Class",
-  //   date: "April 12 & April 19, 2022",
-  //   time: "6:30-8:30",
-  //   img: clay,
-  //   description: "Learn how to make art with clay! To sign up, call (325) 641-2916 or stop by The Art Center between 2 pm and 6 pm weekdays.",
+  //   title: "",
+  //   date: "",
+  //   time: "",
+  //   img: ,
+  //   description: "",
   //   benefit: false,
-  //   price: "$25",
+  //   price: "",
   //   instructor: {
-  //     name: "Christine Brisley-Wade",
-  //     phone: "(512) 693-7032",
+  //     name: "",
+  //     phone: "",
   //     isInstructor: true,
   //   },
   // }
-    // {
-    //   title: "En Plein Air Brownwood Competition - Entry",
-    //   date: "April 2, 2022",
-    //   time: "7:30 am to 9 am",
-    //   img: ducks01,
-    //   description:
-    //     'The event is open to all artists, age 18 and older. Registration will be at the Art Center. Blank canvases will be stamped at this time. Ending time and return to the Art Center will be at 4 pm the same day. One or two plein art artworks created during the event and one studio artwork created at any time of any subject except nudes may be entered in the competition by each artist for a total of 3 works. All works accepted must remain at The Art Center for exhibit and sale through April 23, 2022. Length and width of any frame may not exceed 26". All work must be sturdily framed or gallery wrapped and ready to hang. All work must be for sale. Artists will receive 70% of the sale price of the work, with BAA receieving 30%.',
-    //   benefit: false,
-    //   price: "$20/entry",
-    // },
-    // {
-    //   title: "En Plein Air Brownwood Competition - Exhibit",
-    //   date: "April 7, 2022",
-    //   time: "5 pm to 8 pm",
-    //   img: ducks02,
-    //   description:
-    //     "The award ceremony will begin at 6 pm and the exhibit will remain open from April 7 to April 22",
-    //   benefit: false,
-    // },
+  {
+    title: "Maurine Stewart Exhibit",
+    date: "May 13 - May 27",
+  },
+  {
+    title: "People Painters Group",
+    date: "May 18",
+    time: "1 pm - 3pm"
+  },
+  {
+    title: "Fiber & Texile Artist Group",
+    date: "May 24",
+    time: "2 pm"
+  },
+  {
+    title: "People Painters Group",
+    date: "May 25",
+    time: "1 pm - 3 pm"
+  },
+  {
+    title: "Art Center Closed for Memorial Day",
+    date: "May 30",
+  },
+  {
+    title: "\"The Art of Fiber\" Fiber & Textile Group Exhibit Reception",
+    date: "June 2",
+    time: "5 pm - 8 pm"
+  },
+  {
+    title: "\"For The Love of Color\" Reception",
+    date: "July 7",
+  },
+  {
+    title: "\"Scenes from Brown County\" Exhibit Reception",
+    date: "August 4",
+    time: "5 pm - 8 pm"
+  },
+  {
+    title: "The Eclectic World of Boyd Harris Exhibit Reception",
+    date: "September 1",
+    time: "5 pm - 8 pm"
+  },
+  {
+    title: "Bob Stuth-Wade Exhibit Reception",
+    date: "October 6",
+  },
+  {
+    title: "BAA Photo Group Exhibit Reception",
+    date: "November 3",
+  },
+  {
+    title: "BAA Chili Lunch and Silent Auction Fundraiser",
+    date: "December 1",
+  }
   ];
 
   export default events;
