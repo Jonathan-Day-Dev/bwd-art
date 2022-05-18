64d1db93b8f8044daef823929759d6e83d92fd9e
Jonathan Day <jonday@jonday.dev>
2022-05-18 10:51:55 -0500

    add images to Upcoming Events

--- src/components/Event/components/event_data.js
+++ src/components/Event/components/event_data.js
@@ -1,3 +1,16 @@
+import maurine from "../../../assets/mauselfie.jpg";
+import daniel from "../../../assets/daniel.jpg";
+import peoplepaint from "../../../assets/people_paint.jpg";
+import colors from "../../../assets/colors.jpg";
+import memorial from "../../../assets/memorial.jpg";
+import textile from "../../../assets/textile.jpg";
+import colors2 from "../../../assets/colors2.jpg";
+import bcounty from "../../../assets/bcounty.jpg";
+import world from "../../../assets/world.jpg";
+import bobsw from "../../../assets/bobsw.jpg";
+import camera from "../../../assets/camera.jpg";
+import chili from "../../../assets/chili.jpg";
+
 const events = [
   // {
   //   title: "",
@@ -16,56 +29,68 @@ const events = [
   {
     title: "Maurine Stewart Exhibit",
     date: "May 13 - May 27",
+    img: maurine
   },
   {
     title: "People Painters Group",
     date: "May 18",
-    time: "1 pm - 3pm"
+    time: "1 pm - 3pm",
+    img: peoplepaint
   },
   {
     title: "Fiber & Texile Artist Group",
     date: "May 24",
-    time: "2 pm"
+    time: "2 pm",
+    img: colors
   },
   {
     title: "People Painters Group",
     date: "May 25",
-    time: "1 pm - 3 pm"
+    time: "1 pm - 3 pm",
+    img: daniel
   },
   {
     title: "Art Center Closed for Memorial Day",
     date: "May 30",
+    img: memorial
   },
   {
     title: "\"The Art of Fiber\" Fiber & Textile Group Exhibit Reception",
     date: "June 2",
-    time: "5 pm - 8 pm"
+    time: "5 pm - 8 pm",
+    img: textile
   },
   {
     title: "\"For The Love of Color\" Reception",
     date: "July 7",
+    img: colors2
   },
   {
     title: "\"Scenes from Brown County\" Exhibit Reception",
     date: "August 4",
-    time: "5 pm - 8 pm"
+    time: "5 pm - 8 pm",
+    img: bcounty
   },
   {
     title: "The Eclectic World of Boyd Harris Exhibit Reception",
     date: "September 1",
-    time: "5 pm - 8 pm"
+    time: "5 pm - 8 pm",
+    img: world
   },
   {
     title: "Bob Stuth-Wade Exhibit Reception",
     date: "October 6",
+    img: bobsw
   },
   {
     title: "BAA Photo Group Exhibit Reception",
     date: "November 3",
+    img: camera
   },
   {
     title: "BAA Chili Lunch and Silent Auction Fundraiser",
     date: "December 1",
+    img: chili
   }
   ];
 
