a3aa28c3d8c5c46a0562ee5fa08dd2fa34b859da
Jonathan Day <jonday@jonday.dev>
2022-06-11 18:35:32 -0500

    remove vids from front page

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
                   <Board />
                   <Daniel />
                 </Box>
-                <Features />
+                {/* <Features /> */}
               </Fragment>
             }
           />
