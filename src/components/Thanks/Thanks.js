9b4a2ca6f9fb385b2df50ee05fc3b06d174638c3
Jonathan Day <jonday@jonday.dev>
2022-06-12 10:49:15 -0500

    thanks changes

--- src/components/Thanks/Thanks.js
+++ src/components/Thanks/Thanks.js
@@ -15,17 +15,13 @@ const Thanks = (props) => {
           <Typography variant="h5">Benefactor of the Arts</Typography>
           <Typography variant="h6">Brad &amp; Cathy Bradley</Typography>
           <Typography variant="h6">Boyd &amp; Pat Harris</Typography>
+          <Typography variant="h6">Gayle Massey</Typography>
 
           <Typography variant="h5">Supporter of the Arts</Typography>
           <Typography variant="h6">Jimmie Hudson</Typography>
           <Typography variant="h6">Roger &amp; Linda Levesque</Typography>
           <Typography variant="h6">Keith Clark</Typography>
-
-          <Typography variant="h5">Benefactor</Typography>
-          <Typography variant="h6">Brad &amp; Cathy Bradly</Typography>
-          <Typography variant="h6">Boyd &amp; Pat Harris</Typography>
-          <Typography variant="h6">Gayle Massey</Typography>
-
+          
           <Typography variant="h5">Donors</Typography>
           <Typography variant="h6">Fran Rogers-Sunberg</Typography>
         </div>
