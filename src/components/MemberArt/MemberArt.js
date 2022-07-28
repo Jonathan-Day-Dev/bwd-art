d5df6bb46de2f533b707aea50ce4dd0c123ccac0
Jonathan Day <jonday@jonday.dev>
2022-07-28 13:23:06 -0500

    add new artists

--- src/components/MemberArt/MemberArt.js
+++ src/components/MemberArt/MemberArt.js
@@ -4,17 +4,22 @@ import { useEffect, useRef } from "react";
 import Card from "../UI/Card";
 import Member from "./Member";
 
-import chris_01 from "../../assets/chris_g/01.jpg";
-import chris_02 from "../../assets/chris_g/02.jpg";
-import chris_03 from "../../assets/chris_g/03.jpg";
-import chris_04 from "../../assets/chris_g/04.jpg";
+// import chris_01 from "../../assets/chris_g/01.jpg";
+// import chris_02 from "../../assets/chris_g/02.jpg";
+// import chris_03 from "../../assets/chris_g/03.jpg";
+// import chris_04 from "../../assets/chris_g/04.jpg";
 
-import fred_01 from "../../assets/fred_spencer/01.jpg";
-import fred_02 from "../../assets/fred_spencer/02.jpg";
-import fred_03 from "../../assets/fred_spencer/03.jpg";
-import fred_04 from "../../assets/fred_spencer/04.jpg";
-import fred_05 from "../../assets/fred_spencer/05.jpg";
-import fred_06 from "../../assets/fred_spencer/06.jpg";
+// import fred_01 from "../../assets/fred_spencer/01.jpg";
+// import fred_02 from "../../assets/fred_spencer/02.jpg";
+// import fred_03 from "../../assets/fred_spencer/03.jpg";
+// import fred_04 from "../../assets/fred_spencer/04.jpg";
+// import fred_05 from "../../assets/fred_spencer/05.jpg";
+// import fred_06 from "../../assets/fred_spencer/06.jpg";
+import belli_01 from "../../assets/new/belli2.jpg";
+import belli_02 from "../../assets/new/sunset.jpg";
+
+import christine_01 from "../../assets/new/christine1.jpg";
+import christine_02 from "../../assets/new/christine2.jpg";
 
 import judy_01 from "../../assets/judy_kirksey/01.jpg";
 import judy_02 from "../../assets/judy_kirksey/02.jpg";
@@ -40,16 +45,18 @@ const MemberArt = (props) => {
   }, []);
 
   const memberArt = [
-    {
-      name: "Christopher Gaston",
-      id: "01",
-      art: [chris_01, chris_02, chris_03, chris_04],
-    },
-    {
-      name: "Fred Spencer",
-      id: "02",
-      art: [fred_01, fred_02, fred_03, fred_04, fred_05, fred_06],
-    },
+    // {
+    //   name: "Christopher Gaston",
+    //   id: "01",
+    //   art: [chris_01, chris_02, chris_03, chris_04],
+    // },
+    // {
+    //   name: "Fred Spencer",
+    //   id: "02",
+    //   art: [fred_01, fred_02, fred_03, fred_04, fred_05, fred_06],
+    // },
+    {name: "Martin Belli", id: "01", art: [belli_01, belli_02]},
+    {name: "Christine Brisley", id: "02", art: [christine_01, christine_02]},
     { name: "Judy Kirksey", id: "03", art: [judy_01, judy_02] },
     {
       name: "Julie Mize",
