dc840b91de914f76dc3f6a814804893b6eee3c68
Jonathan Day <jonday@jonday.dev>
2022-08-24 12:30:27 -0500

    add closed gallery for labor day

--- src/components/Layout/Header/Banner.js
+++ src/components/Layout/Header/Banner.js
@@ -99,6 +99,14 @@ const Banner = () => {
             </Typography>
           </Typography>
         </Typography>
+        <Box sx={{
+          backgroundColor: "white",
+          marginTop: "1rem",
+          border: "4px solid blue",
+          padding: "0.5rem",
+          fontWeight: "bold",
+          fontSize: "1.1rem"
+        }}>The gallery will be closed September 5 for Labor Day</Box>
 
         {/* Button Group inside Banner */}
         <Box
