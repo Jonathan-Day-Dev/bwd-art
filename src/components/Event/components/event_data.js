9d43028cb33e29ca15f1f20a29b238cc309a09a3
Jonathan Day <jonday@jonday.dev>
2022-09-09 14:52:51 -0500

    add jserna image

--- src/components/Event/components/event_data.js
+++ src/components/Event/components/event_data.js
@@ -1,8 +1,7 @@
 import bobsw from "../../../assets/bobsw.jpg";
 import camera from "../../../assets/camera.jpg";
 import chili from "../../../assets/chili.jpg";
-// import boyd from "../../../assets/boyd.jpg";
-
+import jserna from "../../../assets/jsernaart.jpg";
 // import {Link} from "react-router-dom";
 
 const events = [
@@ -19,46 +18,6 @@ const events = [
   //     phone: "",
   //     isInstructor: true,
   //   },
-  // {
-  //   title: "The Eclectic World of Boyd Harris Exhibit Reception",
-  //   description: (
-  //     <span>
-  //       "Have you ever met a time traveler? Introduce yourself to Boyd Harris,
-  //       and maybe he’ll share a story from his adventures, as you take in his
-  //       retrospective exhibit, 50 years in the making. Harris describes himself
-  //       as “a geologist by training and an artist by heart.” “History and
-  //       ancient cultures have always fascinated me. I grew up during my
-  //       formative years in New Mexico, a state known for its natural beauty and
-  //       its many artists. This tradition goes back thousands of years to the
-  //       ceramics, rock art, and other cultural materials the indigenous Native
-  //       Americans left us, and make to this day.” As he immersed himself in the
-  //       arts culture as a young man, he also observed the hardship artists had
-  //       in earning income, and resolved to follow a career path in geology that
-  //       offered some financial security. He spent years working in the oil
-  //       fields of the Permian Basin, which he sardonically calls “the gold
-  //       camp,” but stayed active as an artist and art instructor, teaching
-  //       ceramics and sculpture at Midland College. The overlap between his
-  //       geology career and the arts is evident in many of his works which
-  //       include images and abstractions of geological features, folds and faults
-  //       of the earth’s crust, and scenes from the age of the dinosaurs. There
-  //       will also be more traditional paintings of landscapes and seascapes, as
-  //       well as abstracts, monoprints, sgraffito, retablos, and a variety of
-  //       ceramics, including raku, pit fire ceramics, and stone ware. Time
-  //       travel, and the connections between ancient past, present, and far
-  //       future, remain underlying themes of Boyd Harris art. He poses the
-  //       question: “Is the modern artist a Shaman teaching people a new way to
-  //       look at the world around themselves? I ask the patron to make what they
-  //       will of my art in their own mind and hopefully they will learn and enjoy
-  //       it. Maybe it will show them a new way to look at things."-{" "}
-  //       <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
-  //         Daniel Graham
-  //       </span>
-  //     </span>
-  //   ),
-  //   date: "September 1",
-  //   time: "5 pm - 8 pm",
-  //   img: boyd,
-  // },
   {
     title: "Demonstration - Scott Coers",
     description:
@@ -77,7 +36,7 @@ const events = [
     // description: "Scott will be demonstrating how to edit and post personal videos online using free apps",
     date: "October 11",
     time: "6 pm",
-    // img: boyd,
+    img: jserna,
   },
   {
     title: "BAA Photo Group Exhibit Reception",
